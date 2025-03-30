9ce873635972cfef14efc0b9cbe16760ac3ffe9c
Tristan Patout <162041632+3ST1@users.noreply.github.com>
2025-03-30 12:03:39 +0200

    Improved README

--- MessageApp/src/main/java/com/example/accessingdatajpa/Controllers/MessageController.java
+++ MessageApp/src/main/java/com/example/accessingdatajpa/Controllers/MessageController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Messages", description = "Operations endpoints related to messages management in queues and topics")
 public class MessageController {
 
     @Autowired
